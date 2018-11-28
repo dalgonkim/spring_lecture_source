49091da6cec138ad47dfc878f07db19b6c328cc1
kim min <big2-01@clo-09-PC>
2018-11-28 09:13:13 +0900

    modify spring_REST project

--- spring_REST/src/main/java/com/rest/controller/SampleController.java
+++ spring_REST/src/main/java/com/rest/controller/SampleController.java
@@ -1,19 +1,10 @@
-package com.rest.controller;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.rest.dto.SampleVO;
-import com.rest.dto.SampleVOList;
-import com.rest.dto.SampleVOList2;
+import com.spring.dto.SampleVO;
 
 @Controller
 @RequestMapping("/sample")
@@ -28,88 +19,20 @@ public class SampleController {
 		return "jsp";
 	}
 
-	@RequestMapping(value = "/jsonData", method = RequestMethod.GET)
-	@ResponseBody
-	public SampleVO jsonData() throws Exception {
-		SampleVO vo = new SampleVO("kim", "mimi");
-		return vo;
-	}
-
-	// JSON sender : 1 depth
-	@RequestMapping(value = "jsonData", method = RequestMethod.POST)
-	@ResponseBody
-	public List<SampleVO> jsonDataList() throws Exception {
-
-		List<SampleVO> list = new ArrayList<SampleVO>();
-		for (int i = 0; i < 5; i++) {
-			SampleVO vo = new SampleVO("kim" + i, "mimi" + i);
-			list.add(vo);
-		}
-
-		return list;
-	}
-
-	@RequestMapping(value = "/formData", method = RequestMethod.POST)
-	@ResponseBody
-	public SampleVO formData(SampleVO vo, Model model) {
-		System.out.println(vo);
-
-		return vo;
-	}
-
-	// XML sender
-	@RequestMapping(value = "/xml", method = RequestMethod.GET)
-	@ResponseBody
-	public SampleVOList sendXML() {
-		List<SampleVO> sampleList = new ArrayList<SampleVO>();
-		for (int i = 0; i < 5; i++) {
-			SampleVO vo = new SampleVO("kim" + i, "mimi" + i);
-			sampleList.add(vo);
-		}
-
-		SampleVOList voList = new SampleVOList(sampleList);
-
-		return voList;
-	}
+	@RequestMapping("/sendJson")
+	public void sendJson()throws Exception{}
 	
-	//XML->JSON
-	@RequestMapping(value = "/stringToXML", method = RequestMethod.POST)
-	@ResponseBody
-	public SampleVOList2 stringToXML(@RequestBody SampleVOList sampleList) {
-		List<SampleVO> list=sampleList.getSample();
-		
-		SampleVOList2 sampleList2 = new SampleVOList2(list);
-		
-		return sampleList2;
-	}
+	@RequestMapping("/sendJsonArray")
+	public void sendJsonArray(){}
 	
-	// JSON sender : 2 depth
-	@RequestMapping(value="/sendJSON",method=RequestMethod.GET)
-	@ResponseBody
-	public SampleVOList2 senderJSON(){
-		List<SampleVO> sampleList = new ArrayList<SampleVO>();
-		for (int i = 0; i < 5; i++) {
-			SampleVO vo = new SampleVO("kim" + i, "mimi" + i);
-			sampleList.add(vo);
-		}
-		
-		SampleVOList2 sample2=new SampleVOList2(sampleList);
-		
-		return sample2;
-	}
+	@RequestMapping("/sendJsonJson")
+	public void sendJsonJson(){}
 	
-	@RequestMapping(value="/jsonToXML",method=RequestMethod.POST)
-	@ResponseBody
-	public SampleVOList jsonToXML(@RequestBody SampleVOList2 sampleList){
-		
-		List<SampleVO> list=sampleList.getSample();
-		SampleVOList xmlList=new SampleVOList(list);
-		
-		return xmlList;
-	}
+	@RequestMapping("/sendXml")
+	public void sendXml(){}
+	
+	
 }
-
-
 
 
 
--- spring_REST/src/main/java/com/rest/controller/SampleRestController.java
+++ spring_REST/src/main/java/com/rest/controller/SampleRestController.java
@@ -1,92 +1,108 @@
-package com.rest.controller;
+package com.spring.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rest.dto.SampleVO;
+import com.spring.dto.SampleVO;
+import com.spring.dto.SampleVOList;
 
 @RestController
 @RequestMapping("/rs")
 public class SampleRestController {
-	
-	@RequestMapping("/hello")
-	public String sayHello(){
-		return "Hello World";
-	}
-	
-	@RequestMapping("/sendVO")
-	public SampleVO sendVO(){
-		SampleVO vo=new SampleVO("길동","홍");
+
+	@RequestMapping("/jspPage")
+	public String jspPage(Model model) throws Exception {
+
+		SampleVO vo = new SampleVO("kim", "mimi");
+		model.addAttribute("model", vo);
 		
+		return "jsp";
+	}
+
+	@RequestMapping("/jsonData")
+	public SampleVO jsonData() throws Exception {
+		SampleVO vo = new SampleVO("kim", "mimi");
 		return vo;
 	}
-	
-	@RequestMapping("/sendList")
-	public List<SampleVO> sendList(){
-		List<SampleVO> list=new ArrayList<SampleVO>();
-		for(int i=0;i<5;i++){
-			SampleVO vo=new SampleVO("kim"+i, "mimi"+i);
+
+	// JSON sender : 1 depth
+	@RequestMapping(value = "/jsonList", method = RequestMethod.GET)
+	public List<SampleVO> jsonList() throws Exception {
+
+		List<SampleVO> list = new ArrayList<SampleVO>();
+		for (int i = 0; i < 5; i++) {
+			SampleVO vo = new SampleVO("kim" + i, "mimi" + i);
 			list.add(vo);
 		}
-		
+
 		return list;
 	}
 	
-	@RequestMapping("/sendMap")
-	public Map<String,SampleVO> sendMap(){
-		Map<String,SampleVO> map=new HashMap<String,SampleVO>();
+	@RequestMapping(value = "/jsonMap", method = RequestMethod.GET)
+	public Map<String,List<SampleVO>> jsonMap() throws Exception {
 		
-		for (int i=0;i<5;i++){
-			SampleVO vo=new SampleVO("kim"+i,"mimi"+i);
-			map.put(i+"", vo);
-		}
+		Map<String,List<SampleVO>> jsonMap = new HashMap<String,List<SampleVO>>();
 		
-		return map;
-	}
-	
-	@RequestMapping("/sendErrorAuth")
-	public ResponseEntity<Void> sendListAuth(){
-		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-	}
-	
-	@RequestMapping("/sendErrorNot")
-	public ResponseEntity<List<SampleVO>> sendListNot(){
-		List<SampleVO> list=new ArrayList<SampleVO>();
-		for(int i=0;i<5;i++){
-			SampleVO vo=new SampleVO("kim"+i, "mimi"+i);
+		List<SampleVO> list = new ArrayList<SampleVO>();
+		for (int i = 0; i < 5; i++) {
+			SampleVO vo = new SampleVO("kim" + i, "mimi" + i);
 			list.add(vo);
 		}
 		
-		ResponseEntity<List<SampleVO>> entity;
+		jsonMap.put("samples",list);
 		
-		if(list==null){
-			entity=
-			new ResponseEntity<List<SampleVO>>(list,HttpStatus.INTERNAL_SERVER_ERROR);			
-		}else{
-			entity=
-			new ResponseEntity<List<SampleVO>>(list,HttpStatus.OK);
+		return jsonMap;
+	}
+	
+	@RequestMapping("/receiveJson")
+	public String receiveJson(SampleVO sample)throws Exception{
+		System.out.println(sample);
+		
+		return "Success";
+	}
+	
+	@RequestMapping(value="/receiveJsonArray",method=RequestMethod.POST)
+	public String receiveJsonArray(@RequestBody List<SampleVO> sampleList)throws Exception{
+		System.out.println(sampleList);
+		return "Success";
+	}
+	
+	@RequestMapping(value="/receiveJsonMap",method=RequestMethod.POST)
+	public String receiveJsonMap(@RequestBody Map<String,List<SampleVO>> sampleMap)
+									throws Exception{
+		System.out.println(sampleMap);
+		return "Success";
+	}
+	
+	@RequestMapping("/xmlData")
+	public SampleVOList sendXML()throws Exception{
+		List<SampleVO> sampleList = new ArrayList<SampleVO>();
+		for (int i = 0; i < 5; i++) {
+			SampleVO vo = new SampleVO("kim" + i, "mimi" + i);
+			sampleList.add(vo);
 		}
+
+		SampleVOList voList = new SampleVOList();
+		voList.setSampleList(sampleList);
+
+		return voList;
+	}
+	
+	@RequestMapping(value="/receiveXml",method=RequestMethod.POST)
+	public String receiveXml(@RequestBody SampleVOList sampleList){
+		System.out.println(sampleList);
 		
-		return entity;
-		
+		return "Success";
 	}
 }
-
-
-
-
-
-
-
-
-
 
 
 
